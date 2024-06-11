e88fe9fe22268c6d5703441071e5ccf007bed7c8
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2024-06-11 12:04:38 +0000

    Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import tsconfigPaths from 'vite-tsconfig-paths'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
+    tsconfigPaths({ root: './' }), 
     solidPlugin(),
   ],
   server: {
